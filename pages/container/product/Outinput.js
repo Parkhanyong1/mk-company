4d195b5679cff4aa1f817dfbec770ee71fcdde26
Park <ilss9984@gmail.com>
2021-01-15 12:18:15 +0900

    modify product

--- pages/container/product/Outinput.js
+++ pages/container/product/Outinput.js
@@ -12,23 +12,31 @@ function Outinput() {
         <main className={outinputStyle.outinputMain}>
           <h2>아웃풋 기어와 인풋 기어</h2>
           <p>
-            엠케이는 자동차 트랜스미션에 들어가는 미션용 아웃풋기어, 인풋기어를
-            생산하고 있습니다.
+            <strong>
+              엠케이는 자동차 트랜스미션에 들어가는 미션용 아웃풋기어,
+              인풋기어를 생산하고 있습니다.
+            </strong>
           </p>
-          <p>
-            <img
-              src="/output209.jpg"
-              alt="아웃풋 기어 209"
-              title="아웃풋 기어 209"
-            />
-          </p>
-          <p>
-            <img
-              src="/input198.jpg"
-              alt="인풋 기어 198"
-              title="인풋 기어 198"
-            />
-          </p>
+          <div className={outinputStyle.outinputImg}>
+            <p>
+              <img
+                src="/output209.jpg"
+                alt="아웃풋 기어 209"
+                title="아웃풋 기어 209"
+                width="500"
+                height="400"
+              />
+            </p>
+            <p>
+              <img
+                src="/input198.jpg"
+                alt="인풋 기어 198"
+                title="인풋 기어 198"
+                width="500"
+                height="400"
+              />
+            </p>
+          </div>
         </main>
       </section>
     </div>
--- pages/container/product/Ring.js
+++ pages/container/product/Ring.js
@@ -11,28 +11,54 @@ function Ring() {
         </nav>
         <main className={ringStyle.ringMain}>
           <h2>링기어</h2>
-          <p>엠케이는 전기차에 들어가는 변속기용 링기어를 생산하고 있습니다.</p>
-          <p>현재는 8속, 9속을 생산하고 있습니다.</p>
           <p>
-            <img src="/ring169.jpg" alt="링기어 169" title="링기어 169" />
+            <strong>
+              엠케이는 전기차에 들어가는 변속기용 링기어를 생산하고 있습니다.
+            </strong>
           </p>
           <p>
-            <img src="/ring172.jpg" alt="링기어 172" title="링기어 172" />
+            <strong>현재는 8속, 9속을 생산하고 있습니다.</strong>
           </p>
-          <p>
-            <img
-              src="/dvsring08.jpg"
-              alt="dvs 링기어 08"
-              title="dvs 링기어 08"
-            />
-          </p>
-          <p>
-            <img
-              src="/dvsring11.jpg"
-              alt="dvs 링기어 11"
-              title="dvs 링기어 11"
-            />
-          </p>
+          <div className={ringStyle.ringImg}>
+            <p>
+              <img
+                src="/ring169.jpg"
+                alt="링기어 169"
+                title="링기어 169"
+                width="500"
+                height="400"
+              />
+            </p>
+            <p>
+              <img
+                src="/ring172.jpg"
+                alt="링기어 172"
+                title="링기어 172"
+                width="500"
+                height="400"
+              />
+            </p>
+          </div>
+          <div className={ringStyle.ringImg}>
+            <p>
+              <img
+                src="/dvsring08.jpg"
+                alt="dvs 링기어 08"
+                title="dvs 링기어 08"
+                width="500"
+                height="400"
+              />
+            </p>
+            <p>
+              <img
+                src="/dvsring11.jpg"
+                alt="dvs 링기어 11"
+                title="dvs 링기어 11"
+                width="500"
+                height="400"
+              />
+            </p>
+          </div>
         </main>
       </section>
     </div>
